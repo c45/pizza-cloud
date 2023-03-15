ea80ddb32ebb47055e0bbf02c49568b7020c2b1c
residentsIeeper <666pleasekillme@gmail.com>
2023-03-15 23:46:33 +0200

    feat(db) add Ingredient Repository

---
+++ src/main/java/pizza/data/IngredientRepository.java
@@ -0,0 +1,14 @@
+package pizza.data;
+
+import pizza.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
