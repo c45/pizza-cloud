bbfdcf2f9dcea02797fa5b3280703ddd7dd6284d
residentsIeeper <senia27sh2@gmail.com>
2023-03-19 01:15:41 +0200

    ref(data) refactor domain objects to use JPA

--- src/main/java/pizza/Ingredient.java
+++ src/main/java/pizza/Ingredient.java
@@ -1,23 +1,22 @@
 package pizza;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Ingredient {
 
     @Id
-    private final String id;
+    private String id;
 
-    private final String name;
-    private final Type type;
+    private String name;
+    private Type type;
 
     public enum Type {
         DOUGH, VEGETABLES, SAUCES, CHEESE, MEAT
--- src/main/java/pizza/data/IngredientRepository.java
+++ src/main/java/pizza/data/IngredientRepository.java
@@ -3,8 +3,6 @@ package pizza.data;
 import org.springframework.data.repository.CrudRepository;
 import pizza.Ingredient;
 
-import java.util.Optional;
-
 public interface IngredientRepository
         extends CrudRepository<Ingredient, String> {
 }
